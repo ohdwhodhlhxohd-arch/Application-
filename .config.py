bbaecf5e94e56f2f89b8b14c83d54056e04a7863
ohdwhodhlhxohd-arch <ohdwhodhlhxohd@gmail.com>
2026-02-24 01:14:02 +0200

    Add configuration file with API settings

---
+++ .config.py
@@ -0,0 +1,5 @@
+# .config.py
+API_TOKEN = "8172361853:AAFTu6Qf37YJYynQrE4qvB6H9cj0htgCudM"
+PHOTOS_DIR = "photos"
+JSON_FILE = "products.json"
+BASE_URL = "https://your-app.onrender.com"  # غيّره لرابط تطبيقك على Render
