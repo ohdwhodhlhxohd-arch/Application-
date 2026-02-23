cbe50b25bdae8f00789795baf9e74b5ff7a4d4fb
ohdwhodhlhxohd-arch <ohdwhodhlhxohd@gmail.com>
2026-02-24 01:27:57 +0200

    Update BASE_URL to new application endpoint

--- .config.py
+++ .config.py
@@ -2,4 +2,4 @@
 API_TOKEN = "8172361853:AAFTu6Qf37YJYynQrE4qvB6H9cj0htgCudM"
 PHOTOS_DIR = "photos"
 JSON_FILE = "products.json"
-BASE_URL = "https://your-app.onrender.com"  # غيّره لرابط تطبيقك على Render
+BASE_URL = "https://application-4yjw.onrender.com/admin-page"  # غيّره لرابط تطبيقك على Render
